feff316317f4fb446616d44b04ae52b721ddb1c4
Renan Garcia <renangarcia@live.com>
2017-03-07 04:51:05 -0300

    Alterações para o Heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require("./config/express")();
 var rotaIndex = require("./app/routes/index")(app);
 
-app.listen(3000, function() {
+var porta = process.env.PORT || 3000;
+app.listen(porta, function() {
     //console.log("servidor rodando na porta 3000");
 });
